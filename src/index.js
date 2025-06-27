cd5e34abacbd88504a1e2f87562598afb00e2098
thht9010 <kttpo9090@gmail.com>
2025-06-27 16:28:10 +0900

    [docs] 리액트 프로젝트 환경설정

--- src/index.js
+++ src/index.js
@@ -2,16 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+// js 로 root 에 html React 명령으로 그려라
 root.render(
-  <React.StrictMode>
+  // React.StrictMode 는 개발 메세지 여러번 출력하라
+  // <React.StrictMode>
     <App />
-  </React.StrictMode>
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
